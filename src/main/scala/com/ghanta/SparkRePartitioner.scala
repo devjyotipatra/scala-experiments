d1b9257bfafb6977a1562f08475ab8ed9c6820ad
devjyotip <devjyotip@qubole.com>
2017-08-24 16:49:57 +0530

    initial commit

---
+++ src/main/scala/com/ghanta/SparkRePartitioner.scala
@@ -0,0 +1,71 @@
+package com.ghanta
+
+/**
+  * Created by devjyotip on 8/22/17.
+  */
+
+import com.ghanta.SparkRePartitioner.ColumnType.ColumnType
+import com.qubole.tenali.metastore.APIMetastoreClient
+import com.qubole.tenali.metastore.CachingMetastoreClient
+import org.apache.hadoop.hive.metastore.IMetaStoreClient
+import org.apache.hadoop.hive.metastore.api._
+import org.apache.thrift.TException
+
+import collection.JavaConversions
+
+
+object SparkRePartitioner {
+
+  object ColumnType extends Enumeration {
+    type ColumnType = Value
+    val Partitioned, NonPartitioned, None = Value
+  }
+
+  class Columns(columns: Seq[FieldSchema], _type: ColumnType)
+
+  object Columns {
+    def apply(columns: Seq[FieldSchema], _type: ColumnType): Columns = {
+        _type match {
+          case ColumnType.Partitioned => PartitionColumn(columns)
+          case ColumnType.NonPartitioned => NonPartitionColumn(columns)
+          case _ => Columns(Seq(), ColumnType.None)
+        }
+    }
+  }
+
+  case class PartitionColumn(columns: Seq[FieldSchema], val _type: ColumnType = ColumnType.Partitioned)
+    extends Columns(columns, _type)
+
+  case class NonPartitionColumn(columns: Seq[FieldSchema], val _type: ColumnType = ColumnType.NonPartitioned)
+    extends Columns(columns, _type)
+
+
+  class MetaStoreClient(redisEndpoint: String, apiUrl: String, apiToken: String,
+                        TTL_MINS: Int = 1000, MISSINGTTL_MINS: Int = 10000) {
+
+    def getColumns(tableInfo: Table): Seq[FieldSchema] = {
+      JavaConversions.asScalaBuffer(tableInfo.getSd().getCols)
+    }
+
+    def getPartitionColumns(tableInfo: Table): Seq[FieldSchema] = {
+      JavaConversions.asScalaBuffer(tableInfo.getPartitionKeys())
+    }
+
+    @throws(classOf[MetaException])
+    @throws(classOf[TException])
+    @throws(classOf[UnknownTableException])
+    @throws(classOf[UnknownDBException])
+    def getTableSchema(accountId: Int, schema: String, table: String): (Columns, Columns) = {
+      val apimetastoreClient = new APIMetastoreClient(accountId, apiUrl, apiToken)
+      val metastoreClient: IMetaStoreClient = new CachingMetastoreClient(
+                    redisEndpoint, String.valueOf(accountId), TTL_MINS, apimetastoreClient, MISSINGTTL_MINS, true)
+
+      val tableInfo: Table = metastoreClient.getTable(schema, table)
+      val partitionsColumns: Seq[FieldSchema] = getPartitionColumns(tableInfo)
+      val otherColumns: Seq[FieldSchema] = getColumns(tableInfo)
+
+      (Columns(partitionsColumns, ColumnType.Partitioned), NonPartitionColumn(otherColumns, ColumnType.NonPartitioned))
+    }
+  }
+
+}
---
+++ src/test/scala/com/ghanta/SparkRePartitionerTest.scala
@@ -0,0 +1,36 @@
+package ghanta
+
+import com.ghanta.SparkRePartitioner
+import com.ghanta.SparkRePartitioner.{Columns, NonPartitionColumn, PartitionColumn, PartitionColumn}
+import org.apache.hadoop.hive.metastore.api.FieldSchema
+import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.Assertions._
+import org.scalatest.junit.JUnitRunner
+
+/**
+  * Created by devjyotip on 8/22/17.
+  */
+class SparkRePartitionerTest extends FlatSpec with Matchers {
+    val redisEndpoint: String = "mojave-redis.imzqhl.0001.use1.cache.amazonaws.com"
+    val apiUrl: String = "qa3.qubole.net"
+    val apiToken: String = "9VYjFpwMoe3hmzqPfpaS2mFFN7wr4srC8vNT5wXnAxYCGFH3pvtJ4B56ZSPCeu2v"
+
+    val accountId = 5911
+
+    "MetaStoreClient" should "return Partition Keys" in {
+        val client = new SparkRePartitioner.MetaStoreClient(redisEndpoint, apiUrl, apiToken)
+        val columns  = client.getTableSchema(accountId, "tenaliv2", "usagemap")
+
+        val s = columns match {
+          case (p: Columns, np: Columns) => p match {
+            case PartitionColumn(Seq(a: FieldSchema, b: FieldSchema, c: FieldSchema), _) => {
+                Set(a.getName(), b.getName(), c.getName())
+            }
+          }
+        }
+
+        s should contain ("submit_time")
+        s should contain ("account_id")
+        s should contain ("source")
+    }
+}
